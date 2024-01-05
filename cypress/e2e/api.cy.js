b9fe2a0fc7e35949736232d399b5357bbb9a7aa9
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2024-01-05 15:35:36 -0700

    Create a test to check if returns a non-empty array

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -7,7 +7,18 @@ describe("fetchProducts function", () => {
     products = await fetchProducts();
   });
 
+  it("should return a non-empty array", () => {
+    expect(Array.isArray(products), "The result should be an array").to.be.true;
+
+    // The array should have at least one product
+    expect(
+      products.length,
+      "The length of the array should be at least 1"
+    ).to.be.at.least(1);
+  });
+
   it("should return products filtered by specified categories", () => {
+    // Every product should be of these two categories
     const allProductsMatch = products.every(
       (product) =>
         product.category === "jewelery" ||
