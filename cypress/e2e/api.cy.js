9a6b396ffd756bbd5ba551138f6bf9f0ac5309e4
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2024-01-05 15:32:31 -0700

    Refactor the api.cy.js file test and create one test

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,33 +1,22 @@
+import { fetchProducts } from "../../js/api.js";
+
 describe("fetchProducts function", () => {
-  let productsResponse;
-  let filteredProducts;
+  let products;
 
-  beforeEach(() => {
-    cy.request("GET", "https://fakestoreapi.com/products").then((res) => {
-      productsResponse = res;
-      filteredProducts = productsResponse.body.filter(
-        (product) =>
-          product.category === "jewelery" ||
-          product.category === "women's clothing"
-      );
-    });
+  beforeEach(async () => {
+    products = await fetchProducts();
   });
 
-  it("should have a response status of 200", () => {
-    expect(productsResponse.status).to.eq(200);
-  });
+  it("should return products filtered by specified categories", () => {
+    const allProductsMatch = products.every(
+      (product) =>
+        product.category === "jewelery" ||
+        product.category === "women's clothing"
+    );
 
-  it("should return products filtered by specified categories when API is successful", () => {
-    const randomIndex = Math.floor(Math.random() * filteredProducts.length);
-    const randomProduct = filteredProducts[randomIndex];
-
-    expect(randomProduct.category).to.be.oneOf([
-      "jewelery",
-      "women's clothing",
-    ]);
-  });
-
-  it("should return a filtered array of 10 products when API is successful", () => {
-    expect(filteredProducts).to.have.lengthOf(10);
+    expect(
+      allProductsMatch,
+      "All products should be in the specified categories"
+    ).to.be.true;
   });
 });
